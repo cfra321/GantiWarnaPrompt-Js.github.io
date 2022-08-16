65112a33902b186b964c911801c196c861b026ea
ku2h_wicaksono <93067182+cfra321@users.noreply.github.com>
2022-08-16 16:37:58 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+unction myFunction() {
+                var warna = prompt("Ketikan Warna yang kamu inginkan(English)","yellow")
+                if (warna != null){
+                document.body.style.backgroundColor = warna ;
+                    
+                }
+            }
